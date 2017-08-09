fdc3840b487ca9c8530dc74120584dbd829562ca
Ewan Dawson <ewan.dawson@symphonicsoft.com>
2017-08-09 23:09:38 +0100

    Demonstrate quasar is working with simple example

--- webscript-quasar/src/main/kotlin/net/lazygun/webscript/Application.kt
+++ webscript-quasar/src/main/kotlin/net/lazygun/webscript/Application.kt
@@ -1,9 +1,19 @@
 package net.lazygun.webscript
 
+import co.paralleluniverse.kotlin.fiber
+import co.paralleluniverse.strands.Strand
+
 /**
  * @author Ewan
  */
 
 fun main(args: Array<String>) {
-
+    (1..5).forEach { i ->
+        fiber {
+            (1..10).forEach {
+                println(String.format("$i-%02d : Hello!", it))
+                Strand.sleep(100)
+            }
+        }
+    }
 }

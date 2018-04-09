2cf6c823a1b6097ab8a5292c76d7cb51ffa7f9a0
Ewan Dawson <ewan.dawson@symphonicsoft.com>
2018-04-09 23:04:32 +0100

    Camel module changes

---
+++ camel/src/main/kotlin/Interface.kt
@@ -0,0 +1,21 @@
+/**
+ * @author Ewan
+ */
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(factory = Netty, port = 7777) {
+        routing {
+            get("/") {
+                call.respondText("Hello world!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
+}
